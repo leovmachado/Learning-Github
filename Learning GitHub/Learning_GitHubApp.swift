3193c5b130b790337d2abb38106f2aa459c48e1a
Leonardo <leozin.vieira.lv@gmail.com>
2022-01-13 23:35:10 -0800

    Initial Commit

---
+++ Learning GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning GitHub
+//
+//  Created by Leonardo Machado on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
